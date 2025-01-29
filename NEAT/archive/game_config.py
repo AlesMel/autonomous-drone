81e1be44728c70bb48b653337a08ae0423267c4d
Aleš Melichar <alesmelichar13@gmail.com>
2025-01-29 17:20:34 +0100

    Clean-up

--- NEAT/archive/game_config.py
+++
@@ -1,9 +0,0 @@
-game_config = {
-    "is_training": False,
-    "no_graphics": False,
-    "is_debug": False,
-    "load_from_checkpoint": False,
-    "max_generations": 200,
-    "single_agent_env_path": "./Builds/SingleAgent/3DBallBalancing.exe",
-    "multi_agent_env_path": "./Builds/60Agents/3DBallBalancing.exe",
-}
--- NEAT/neat_trainer copy 3.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from mlagents_envs.environment import UnityEnvironment
-
-class UnityGym():
-    def __init__(
-            self,
-            unity_env: UnityEnvironment
-            ):
-        self._env = unity_env
